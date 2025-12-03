381f3bd577f5404c2832f0552b5fd28ad322a60f
Ramalakshmi-AR <ramyarani.a@gmail.com>
2025-12-03 11:54:09 +0530

    Add Cloudinary media storage

--- Majestic_Manor/settings.py
+++ Majestic_Manor/settings.py
@@ -39,8 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    # your apps
     'book',
     'billing',
     'hospitality',
@@ -48,6 +46,17 @@ INSTALLED_APPS = [
     'contact',
     'about',
 ]
+
+
+INSTALLED_APPS += ["cloudinary", "cloudinary_storage"]
+
+DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
+
+CLOUDINARY_STORAGE = {
+    "CLOUD_NAME": os.getenv("CLOUD_NAME"),
+    "API_KEY": os.getenv("CLOUD_API_KEY"),
+    "API_SECRET": os.getenv("CLOUD_API_SECRET"),
+}
 
 # --------------------------
 # MIDDLEWARE
